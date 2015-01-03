e1ebe2e911d1c65b90be1e54b69338f208834c08
flosell <florian.sellmayr@gmail.com>
2015-01-03 19:05:28 +0100

    deploying dummy-application to aws using scripts based on devops-101

---
+++ resources/deploy-new-app-server.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'json'
+
+def extract_subnet_id
+  describe_subnets_command = "aws ec2 describe-subnets \
+                              --filters Name=tag:aws:cloudformation:logical-id,Values=Subnet \
+                              --region eu-west-1 \
+                              --output json"
+  subnets = JSON.parse(`#{describe_subnets_command}`)["Subnets"]
+  subnet_id = subnets.first["SubnetId"]
+  puts "Extracted subnet id: #{subnet_id}"
+  return subnet_id
+end
+
+def extract_security_group_id
+  describe_security_groups_command = "aws ec2 describe-security-groups \
+                                      --filters Name=tag:aws:cloudformation:logical-id,Values=SecurityGroup \
+                                      --region eu-west-1 \
+                                      --output json"
+  security_groups = JSON.parse(`#{describe_security_groups_command}`)["SecurityGroups"]
+  security_group_id = security_groups.first["GroupId"]
+  puts "Extracted security group id: #{security_group_id}"
+  return security_group_id
+end
+
+def commence_creation_of_stack(s3_uri, subnet_id, security_group_id,build_id)
+  puts "Commencing creation of stack: app-server-#{build_id}"
+  `aws cloudformation create-stack \
+  --stack-name app-server-#{build_id} \
+  --template-body "file://./app-server-template.json" \
+  --region eu-west-1 \
+  --output text \
+  --parameters \
+  ParameterKey=SubnetId,ParameterValue=#{subnet_id} \
+  ParameterKey=SecurityGroupId,ParameterValue=#{security_group_id} \
+  ParameterKey=ArtifactUri,ParameterValue=#{s3_uri} \
+  --capabilities CAPABILITY_IAM`
+end
+
+def check_that_the_stack_has_been_created(s3_uri,build_id)
+  describe_stacks_command = "aws cloudformation describe-stacks \
+                             --stack-name app-server-#{build_id} \
+                             --region eu-west-1 \
+                             --output json"
+  stacks = JSON.parse(`#{describe_stacks_command}`)["Stacks"]
+  stack_status = stacks.first["StackStatus"]
+
+  puts "Awaiting creation of app-server-#{build_id} with status of #{stack_status}"
+
+  if stack_status == "CREATE_COMPLETE"
+    puts "Stack creation complete"
+    exit 0
+  elsif stack_status != "CREATE_IN_PROGRESS"
+    puts "Stack creation failed"
+    exit 1
+  end
+end
+
+def main(s3_uri,build_id)
+  subnet_id = extract_subnet_id
+  security_group_id = extract_security_group_id
+  commence_creation_of_stack(s3_uri, subnet_id, security_group_id,build_id)
+  loop do
+    check_that_the_stack_has_been_created(s3_uri,build_id)
+    sleep(15)
+  end
+end
+
+main(ARGV[0],ARGV[1])
--- src/devops_101_pipeline/steps.clj
+++ src/devops_101_pipeline/steps.clj
@@ -3,7 +3,9 @@
             [lambdacd.execution :as execution]
             [lambdacd.git :as git]
             [lambdacd.manualtrigger :as manualtrigger]
-            [clojure.core.strint :as s]))
+            [clojure.core.strint :as s]
+            [clojure.java.io :as io]
+            [lambdacd.util :as util]))
 
 (def devops-101-repo "git@github.com:flosell/devops-101.git")
 
@@ -16,7 +18,8 @@
 (defn commit-step [{cwd :cwd revision :revision} ctx]
   (let [app-folder (str cwd "/part-four/application")
         timestamp (System/currentTimeMillis)
-        jar-file (s/<< "application-~{revision}-~{timestamp}-standalone.jar")
+        build-id (s/<< "~{revision}-~{timestamp}")
+        jar-file (s/<< "application-~{build-id}-standalone.jar")
         s3-address (s/<< "s3://devops-101-lambdacd/~{jar-file}")
         shell-result (shell/bash ctx app-folder
                                  "set -x"
@@ -24,7 +27,24 @@
                                  "lein uberjar"
                                  (s/<< "mv -v target/uberjar/application*-standalone.jar ~{jar-file}")
                                  (s/<< "aws s3 cp ~{jar-file} ~{s3-address}"))]
-    (assoc shell-result :s3-address s3-address)))
+    (assoc shell-result :s3-address s3-address :build-id build-id)))
 
-(defn deploy-step [{s3-address :s3-address} ctx]
-  {:status :success :out (str "Deploying to " s3-address)})
+
+(defn copy-to [dir resource-name]
+  (spit (str dir "/" resource-name) (slurp (io/resource resource-name))))
+
+(defn prepare-deploy-scripts []
+  (let [basedir (util/create-temp-dir)]
+    (copy-to basedir "app-server-template.json")
+    (copy-to basedir "deploy-new-app-server.rb")
+    (copy-to basedir "retire-old-app-server.rb")
+   basedir))
+
+(defn deploy [s3-address build-id ctx]
+  (let [basedir (prepare-deploy-scripts)]
+    (shell/bash ctx basedir
+                (str "/usr/local/bin/ruby deploy-new-app-server.rb " s3-address " " build-id)
+                (str "/usr/local/bin/ruby retire-old-app-server.rb " build-id))))
+
+(defn deploy-step [{s3-address :s3-address build-id :build-id} ctx]
+  (deploy s3-address build-id ctx))
