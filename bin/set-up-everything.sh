cc2f5943f665f36f3b757848b39bce82bb6ab30e
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-05-10 19:52:29 +0200

    refactor setup script

--- bin/set-up-everything.sh
+++ bin/set-up-everything.sh
@@ -17,33 +17,45 @@ function droplet_ip {
   tugboat info --name $1 | grep "IP:" | awk '{print $2}'
 }
 
+DEPLOY_KEY_ID=$(deploy_key_id)
 
-if ! droplet_present ${APP_DROPLET_NAME}; then
-  echob "Spinning up new instance to hold your application"
-  DEPLOY_KEY_ID=$(deploy_key_id)
-  tugboat create ${APP_DROPLET_NAME} --image ${UBUNTU_IMAGE_ID} --size ${SMALL_INSTANCE_SIZE} --keys ${DEPLOY_KEY_ID}
-  tugboat wait --name ${APP_DROPLET_NAME}
-fi
+function start_droplet {
+  NAME=$1
+  INSTANCE_SIZE=$2
+  if ! droplet_present ${NAME}; then
+    echob "Spinning up new instance ${NAME}"
+    tugboat create ${NAME} --image ${UBUNTU_IMAGE_ID} --size ${INSTANCE_SIZE} --keys ${DEPLOY_KEY_ID}
+    tugboat wait --name ${NAME}
+  fi
 
+  DROPLET_IP=$(droplet_ip ${APP_DROPLET_NAME})
+
+  echo -n "Waiting for ssh to become available"
+
+  ssh-keygen -R ${DROPLET_IP} > /dev/null 2>&1;
+  while ! ssh -i ~/.ssh/deploy_key root@${DROPLET_IP}  exit > /dev/null 2>&1; do
+    echo -n "."
+    sleep 1
+  done
+}
+
+start_droplet ${APP_DROPLET_NAME} ${SMALL_INSTANCE_SIZE}
 APP_DROPLET_IP=$(droplet_ip ${APP_DROPLET_NAME})
+
 echob "Droplet ${APP_DROPLET_NAME} ready to go at ${APP_DROPLET_IP}"
 
 echob "Provisioning App Server..."
 ansible-playbook  --inventory "${APP_DROPLET_IP}," --private-key ${DEPLOY_PRIVATE_KEY} --user root ../ansible/app-playbook.yml
 
 
-if ! droplet_present ${CI_DROPLET_NAME}; then
-  echob "Spinning up new instance to hold LambdaCD"
-  tugboat create ${CI_DROPLET_NAME} --image ${UBUNTU_IMAGE_ID} --size ${BIG_INSTANCE_SIZE}
-  tugboat wait --name ${CI_DROPLET_NAME}
-fi
 
+start_droplet ${CI_DROPLET_NAME} ${BIG_INSTANCE_SIZE}
 CI_DROPLET_IP=$(droplet_ip ${CI_DROPLET_NAME})
 echob "Droplet ${CI_DROPLET_NAME} ready to go at ${CI_DROPLET_IP}"
 echob
 
 echob "Provisioning CI Server..."
-ansible-playbook  --inventory "${CI_DROPLET_IP}," -u root --extra-vars "app_server_ip=${APP_DROPLET_IP}" ../ansible/ci-playbook.yml
+ansible-playbook  --inventory "${CI_DROPLET_IP}," --private-key ${DEPLOY_PRIVATE_KEY} -u root --extra-vars "app_server_ip=${APP_DROPLET_IP}" ../ansible/ci-playbook.yml
 
 echob
 echob "Your CI Machine is provisioned successfully."
@@ -52,6 +64,6 @@ echob "Your CI Machine is provisioned successfully."
 echob "Success!"
 echob "=============================================="
 echob
-echob "App-Server:\t $APP_DROPLET_IP"
-echob "CI-Server: \t $CI_DROPLET_IP"
+echob "App-Server: $APP_DROPLET_IP"
+echob "CI-Server:  $CI_DROPLET_IP"
 
