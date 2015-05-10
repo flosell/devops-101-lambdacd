3ea7a1cdff7e0b7877a1ef4242aca2a5fb4215d4
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-05-10 14:55:17 +0200

    uberjar is in different folder

--- resources/deployscripts/deploy-app.sh
+++ resources/deployscripts/deploy-app.sh
@@ -5,9 +5,10 @@ SCRIPT_DIR=$(dirname $0)
 
 cd ${SCRIPT_DIR}
 
-mv target/*-standalone.jar app.jar
+mv target/uberjar/*-standalone.jar app.jar
+
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo "Starting deployment"
-
 ansible-playbook  -i "app-server," -u root deploy-app.yml
 
